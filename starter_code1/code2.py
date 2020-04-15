30040d069d46f2d4c4150a3feb7812ff0d559065
kobe <jiangq113@163.com>
2020-04-16 00:01:03 +0800

    词干提取，去除停用词。。。

--- starter_code1/code2.py
+++ starter_code1/code2.py
@@ -1,5 +1,9 @@
 import json
 from collections import Counter
+from queue import PriorityQueue
+
+import matplotlib.pyplot as plt
+
 
 def read_corpus(filepath):
     """
@@ -31,13 +35,150 @@ def read_corpus(filepath):
     return qlist, alist
 
 
+# TODO: 统计一下在qlist 总共出现了多少个单词？ 总共出现了多少个不同的单词？
+#       这里需要做简单的分词，对于英文我们根据空格来分词即可，其他过滤暂不考虑（只需分词）
+
 qlist, alist = read_corpus('./data/train-v2.0.json')
 
-q_all_list = []
+# q_all_list = []
+#
+# for q in qlist:
+#     q_list = q.strip(' .!?').split()
+#     q_all_list.extend(q_list)
+
+word_count = Counter()
 
 for q in qlist:
     q_list = q.strip(' .!?').split()
-    q_all_list.append(q_list)
+    word_count.update(q_list)
 
-print(len(q_all_list))
-print(len(Counter(q_all_list).keys()))
+print(len(word_count.keys()))
+print(sum(word_count.values()))
+
+# TODO: 统计一下qlist中每个单词出现的频率，并把这些频率排一下序，然后画成plot. 比如总共出现了总共7个不同单词，而且每个单词出现的频率为 4, 5,10,2, 1, 1,1
+#       把频率排序之后就可以得到(从大到小) 10, 5, 4, 2, 1, 1, 1. 然后把这7个数plot即可（从大到小）
+#       需要使用matplotlib里的plot函数。y轴是词频
+
+value_sort = sorted(word_count.values(), reverse=True)
+plt.subplot(221)
+plt.plot(value_sort)
+plt.subplot(222)
+plt.plot(value_sort[:2000])
+plt.subplot(223)
+plt.plot(value_sort[:200])
+plt.subplot(224)
+plt.plot(value_sort[:20])
+
+inverse_dict = dict(zip(word_count.values(), word_count.keys()))
+list_20 = [[inverse_dict[num], num] for num in value_sort[:20]]
+
+print(list_20)
+
+# TODO: 对于qlist, alist做文本预处理操作。 可以考虑以下几种操作：
+#       1. 停用词过滤 （去网上搜一下 "english stop words list"，会出现很多包含停用词库的网页，或者直接使用NLTK自带的）
+#       2. 转换成lower_case： 这是一个基本的操作
+#       3. 去掉一些无用的符号： 比如连续的感叹号！！！， 或者一些奇怪的单词。
+#       4. 去掉出现频率很低的词：比如出现次数少于10,20....
+#       5. 对于数字的处理： 分词完只有有些单词可能就是数字比如44，415，把所有这些数字都看成是一个单词，这个新的单词我们可以定义为 "#number"
+#       6. stemming（利用porter stemming): 因为是英文，所以stemming也是可以做的工作
+#       7. 其他（如果有的话）
+#       请注意，不一定要按照上面的顺序来处理，具体处理的顺序思考一下，然后选择一个合理的顺序
+#  hint: 停用词用什么数据结构来存储？ 不一样的数据结构会带来完全不一样的效率！
+
+
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+import math
+
+# 加载nltk的自带停用词
+sw = set(stopwords.words('english'))
+# 个人感觉对于一个问题而言这些词不应该删去？？？？
+# sw -= {'who', 'when', 'why', 'where', 'how', 'which'}
+# 这里只是随便去了下符号
+sw.update(["\'s", "‘’", "\'\'"])
+ps = PorterStemmer()
+
+
+def text_preprocessing(text):
+    seg = list()
+    # 直接使用nltk分词
+    for word in word_tokenize(text):
+        # 小写化、词干提取
+        word = ps.stem(word.lower())
+        # 数值归一
+        word = '#number' if word.isdigit() else word
+        if len(word) > 1 and word not in sw:
+            seg.append(word)
+
+    return seg
+
+
+words_cnt = Counter()
+qlist_seg = list()
+for text in qlist:
+    seg = text_preprocessing(text)
+    qlist_seg.append(seg)
+    words_cnt.update(seg)
+
+value_sort = sorted(words_cnt.values(), reverse=True)
+
+# 根据Zipf定律计算99%覆盖率下的过滤词频
+min_tf = value_sort[int(math.exp(0.99 * math.log(len(words_cnt))))]
+
+# 对于每个句子，去掉一些低频词语
+for cur in range(len(qlist_seg)):
+    qlist_seg[cur] = [word for word in qlist_seg[cur] if words_cnt[word] > min_tf]
+
+# TODO: 把qlist中的每一个问题字符串转换成tf-idf向量, 转换之后的结果存储在X矩阵里。 X的大小是： N* D的矩阵。 这里N是问题的个数（样本个数），
+#       D是字典库的大小。
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vectorizer = TfidfVectorizer()  # 定义一个tf-idf的vectorizer
+X = vectorizer.fit_transform([' '.join(seg) for seg in qlist_seg])  # 结果存放在X矩阵
+
+
+# # TODO: 矩阵X有什么特点？ 计算一下它的稀疏度
+def sparsity_ratio(X):
+    return 1.0 - X.nnz / float(X.shape[0] * X.shape[1])
+
+
+print(X.shape)
+print("input sparsity ratio:", sparsity_ratio(X))  # 打印稀疏度(sparsity)
+
+
+def top5results(input_q):
+    """
+    给定用户输入的问题 input_q, 返回最有可能的TOP 5问题。这里面需要做到以下几点：
+    1. 对于用户的输入 input_q 首先做一系列的预处理，然后再转换成tf-idf向量（利用上面的vectorizer)
+    2. 计算跟每个库里的问题之间的相似度
+    3. 找出相似度最高的top5问题的答案
+    """
+    q_vector = vectorizer.transform([' '.join(text_preprocessing(input_q))])
+    # 计算余弦相似度,tfidf默认使用L2范数；矩阵乘法
+    sim = (X * q_vector.T).toarray()
+    # 使用优先队列找出top5，最先出来的是相似度小的
+    pq = PriorityQueue()
+    for cur in range(sim.shape[0]):
+        pq.put((sim[cur][0], cur))
+        if len(pq.queue) > 5:
+            pq.get()
+
+    pq_rank = sorted(pq.queue, reverse=True, key=lambda x: x[0])
+    top_idxs = [x[1] for x in pq_rank]  # top_idxs存放相似度最高的(存在qlist里的)问题的下表
+                                        # hint: 利用priority queue来找top results
+
+    return [alist[i] for i in top_idxs]  # 返回相似度最高的问题对应的答案，作为TOP5答案
+
+# TODO: 编写几个测试用例，并输出结果
+print(top5results("Which airport was shut down?"))    # 在问题库中存在，经过对比，返回的首结果正确
+print(top5results("Which airport is closed?"))
+print(top5results("What government blocked aid after Cyclone Nargis?"))
+print(top5results("Which government stopped aid after Hurricane Nargis?"))
+
+
+
+
+
+plt.show()
