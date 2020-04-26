566cfb378bb1113fb76b502463f3ffc60a02d057
kobe <jiangq113@163.com>
2020-04-27 00:14:53 +0800

    压缩字符串

---
+++ starter_code1/test05.py
@@ -0,0 +1,21 @@
+# dict1 = {'a': 1, 'b': 2, 'c': 2}
+# new_s = []
+# new_s = str([s for s, num in dict1.items()])
+
+
+def compressString(S):
+    dict1 = []
+    head = 0
+    tail = 0
+    prev = S[0]
+    len1 = len(S)
+    for i in range(len1):
+        if S[i] != prev:
+            dict1.append(S[head:i])
+            prev = s
+            head = tail
+
+
+
+res = compressString("aabcccccaa")
+print(res)
